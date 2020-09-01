346d362ad21703fc7f3729a703fc09e2ea75297c
Petko Yovchevski <pyovchevski@gmail.com>
2020-09-01 18:06:20 +0300

    updates

--- settings.php
+++ settings.php
@@ -24,6 +24,7 @@ if (isset($params['for_module_id'])) {
 
     <div class="form-group">
         <label class="control-label"><?php _e("Redirect to URL after submit"); ?></label>
+        <small class="text-muted d-block mb-2">For example: redirect to thank_you.php page</small>
         <input name="newsletter_redirect_after_submit"   option-group="<?php print $mod_id ?>"    value="<?php print get_option('newsletter_redirect_after_submit', $mod_id); ?>" class="mw_option_field form-control" type="text" />
     </div>
 </div>
