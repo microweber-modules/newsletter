2ce52937b0aea069eaf9284d4794d57f43097874
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-12-07 12:11:23 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $config['ui'] = true;
 $config['ui_admin'] = true;
 $config['categories'] = 'marketing';
 $config['position'] = 55;
-$config['version'] = 1.3;
+$config['version'] = 1.4;
 
 $config['tables'] = array (
 		
